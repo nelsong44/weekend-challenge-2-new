b89ea8aaf605dba06455015bd6ef7e15e654654d
nelsong44 <nelsong44@gmail.com>
2017-07-03 17:18:43 -0500

    route created and linked

---
+++ server/routes/operation.js
@@ -0,0 +1,8 @@
+//need express-the hub that allows us to route things to different places
+var express = require('express');
+//a constructor method>> //creating a router for us we can use
+var router = express.Router();
+
+console.log('router file linked');
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('express'); //require the node express module
-// var operation = require('./routes/operation.js');
+var operation = require('./routes/operation.js');
 var app = express(); //call express function to create an instance of the obj
 var port = 5000; //establish a port
 var path = require('path');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:true}));
-// app.use('/operaton', operation);
+app.use('/operaton', operation);
 
 app.get('/*', function(req, res) {
   var file = req.params[0] || 'views/index.html'; //will load html file if no other file is specified
